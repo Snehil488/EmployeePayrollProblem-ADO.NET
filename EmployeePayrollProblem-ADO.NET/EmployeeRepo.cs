ee1cee1d266f7852636e3807c73ac8bbd04dede2
Snehil <snehilverma488@gmail.com>
2020-11-06 18:44:48 +0530

    [Snehli] Add : Estabilishing Connection with sql server database

---
+++ EmployeePayrollProblem-ADO.NET/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollProblem_ADO.NET
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=LAPTOP-T69UTPHB\SQLEXPRESS;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
